d77c2a72ea398e3762337591f91aff6e516a388c
Zakwan Dadabhai <zakwandadabhai@gmail.com>
2025-06-30 19:22:37 -0400

    added chat controller and route

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
-//import chatRoutes from "./routes/chat.route.js";
+import chatRoutes from "./routes/chat.route.js";
 
 import {connectDB} from "./lib/db.js";
 
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser()); 
 app.use("/api/auth", authRoutes);
 app.use("api/user", userRoutes);
-//app.use("/api/chat", chatRoutes);
+app.use("/api/chat", chatRoutes);
 
 
 app.listen(PORT, () => {
