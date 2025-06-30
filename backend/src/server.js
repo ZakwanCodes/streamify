2c02c67d1e5fb061fd2d93b233dbd2857de3b081
Zakwan Dadabhai <zakwandadabhai@gmail.com>
2025-06-30 18:42:45 -0400

    added friends req routes and finished it

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,9 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+//import chatRoutes from "./routes/chat.route.js";
+
 import {connectDB} from "./lib/db.js";
 
 const app = express();
@@ -11,7 +14,9 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser()); 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("api/user", userRoutes);
+//app.use("/api/chat", chatRoutes);
 
 
 app.listen(PORT, () => {
